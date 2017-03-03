d855ec4c9ec6da7a5847d44010dcf08660c081b2
CoderKnot <coder.knot@gmail.com>
2017-03-03 09:31:56 -0800

    include cost for each size added to base cost for calculating cost of pizza

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,21 @@ function Pizza(size, toppings) {
 Pizza.prototype.calculateCost = function() {
   var baseCost = 8;
 
-  this.cost = baseCost;
+  this.cost = baseCost + this.getSizeCost();
+}
+
+Pizza.prototype.getSizeCost = function() {
+  if(this.size === 8) {
+    return 0;
+  } else if(this.size === 10) {
+    return 2;
+  } else if(this.size === 12) {
+    return 4;
+  } else if(this.size === 14) {
+    return 6;
+  } else if(this.size === 16) {
+    return 8;
+  }
 }
 
 var pSize = 10;
