afeeb74dfd3aa277778283be938f772a78d5228c
CoderKnot <coder.knot@gmail.com>
2017-03-03 13:55:24 -0800

    tie user interface to business logic by collecting input from form fields to create order in index.html and scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -37,18 +37,19 @@ Pizza.prototype.getToppingsCost = function() {
 Pizza.prototype.getCost = function() {
   return this.cost;
 }
+
 //Order
-function Order(name, pizzas, delivery) {
+function Order(name, pizzas, isDelivery) {
   this.name = name;
   this.pizzas = pizzas;
   this.totalCost = 0;
-  this.isDelivery = delivery;
+  this.isDelivery = isDelivery;
   this.address = [];
 }
 
 Order.prototype.calculateCost = function() {
-  for(var i = 0; i < pizzas.length; i++) {
-    this.totalCost += pizzas[i].getCost();
+  for(var i = 0; i < this.pizzas.length; i++) {
+    this.totalCost += this.pizzas[i].getCost();
   }
 }
 
@@ -58,85 +59,64 @@ Order.prototype.setAddress = function(address) {
   }
 }
 
-var pSize = 10;
-var pToppings = ["extra cheese","pepperoni"];
-var pSize2 = 12;
-var pToppings2 = ["ham"];
-var pizzas = [];
-
-var newPizza = new Pizza(pSize, pToppings);
-newPizza.calculateCost();
-pizzas.push(newPizza);
-
-var newPizza2 = new Pizza(pSize2, pToppings2);
-newPizza2.calculateCost();
-pizzas.push(newPizza2);
-
-var name = "Chris";
-var deliveryOption = true;
-var street = "401 Frantz Avnenue";
-var city = "Mullens";
-var state = "WV";
-var zip = "25882";
-var address = [street, city, state, zip];
-
-var newOrder = new Order(name, pizzas, deliveryOption);
-
+// Other
 function addFields() {
-  $("#pizzas").append('<div class="panel panel-info">' +
-    '<div class="panel-heading">' +
-      '<h2 class="panel-title">Size</h2>' +
+  $("#pizzas").append('<div class="pizza">' +
+    '<div class="panel panel-info">' +
+      '<div class="panel-heading">' +
+        '<h2 class="panel-title">Size</h2>' +
+      '</div>' +
+      '<div class="panel-body">' +
+        '<div class="radio">' +
+          '<label>' +
+            '<input type="radio" name="size" value="8" checked>' +
+            '8"' +
+          '</label>' +
+        '</div>' +
+        '<div class="radio">' +
+          '<label>' +
+            '<input type="radio" name="size" value="10">' +
+            '10"' +
+          '</label>' +
+        '</div>' +
+        '<div class="radio">' +
+          '<label>' +
+            '<input type="radio" name="size" value="12">' +
+            '12"' +
+          '</label>' +
+        '</div>' +
+        '<div class="radio">' +
+          '<label>' +
+            '<input type="radio" name="size" value="14">' +
+            '14"' +
+          '</label>' +
+        '</div>' +
+        '<div class="radio">' +
+          '<label>' +
+            '<input type="radio" name="size" value="16">' +
+            '16"' +
+          '</label>' +
+        '</div>' +
+      '</div>' +
     '</div>' +
-    '<div class="panel-body">' +
-      '<div class="radio">' +
-        '<label>' +
-          '<input type="radio" name="size" value="8" checked>' +
-          '8"' +
-        '</label>' +
+    '<div class="panel panel-info">' +
+      '<div class="panel-heading">' +
+        '<h2 class="panel-title">Toppings</h2>' +
       '</div>' +
-      '<div class="radio">' +
-        '<label>' +
-          '<input type="radio" name="size" value="10">' +
-          '10"' +
-        '</label>' +
-      '</div>' +
-      '<div class="radio">' +
-        '<label>' +
-          '<input type="radio" name="size" value="12">' +
-          '12"' +
-        '</label>' +
-      '</div>' +
-      '<div class="radio">' +
-        '<label>' +
-          '<input type="radio" name="size" value="14">' +
-          '14"' +
-        '</label>' +
-      '</div>' +
-      '<div class="radio">' +
-        '<label>' +
-          '<input type="radio" name="size" value="16">' +
-          '16"' +
-        '</label>' +
-      '</div>' +
-    '</div>' +
-  '</div>' +
-  '<div class="panel panel-info">' +
-    '<div class="panel-heading">' +
-      '<h2 class="panel-title">Toppings</h2>' +
-    '</div>' +
-    '<div class="panel-body">' +
-      '<div class="form-group">' +
-        '<input type="checkbox" name="toppings" value="extra-cheese">Extra Cheese</input>' +
-        '<input type="checkbox" name="toppings" value="pepperoni">Pepperoni</input>' +
-        '<input type="checkbox" name="toppings" value="ham">Ham</input>' +
-        '<input type="checkbox" name="toppings" value="sausage">Sausage</input>' +
-        '<input type="checkbox" name="toppings" value="bacon">Bacon</input>' +
-        '<input type="checkbox" name="toppings" value="bell-pepper">Bell Pepper</input>' +
-        '<input type="checkbox" name="toppings" value="onion">Onion</input>' +
-        '<input type="checkbox" name="toppings" value="mushroom">Mushrooms</input>' +
-        '<input type="checkbox" name="toppings" value="green-olive">Green Olive</input>' +
-        '<input type="checkbox" name="toppings" value="black-olive">Black Olive</input>' +
-        '<input type="checkbox" name="toppings" value="pineapple">Pineapple</input>' +
+      '<div class="panel-body">' +
+        '<div class="form-group">' +
+          '<input type="checkbox" name="toppings" value="extra-cheese">Extra Cheese</input>' +
+          '<input type="checkbox" name="toppings" value="pepperoni">Pepperoni</input>' +
+          '<input type="checkbox" name="toppings" value="ham">Ham</input>' +
+          '<input type="checkbox" name="toppings" value="sausage">Sausage</input>' +
+          '<input type="checkbox" name="toppings" value="bacon">Bacon</input>' +
+          '<input type="checkbox" name="toppings" value="bell-pepper">Bell Pepper</input>' +
+          '<input type="checkbox" name="toppings" value="onion">Onion</input>' +
+          '<input type="checkbox" name="toppings" value="mushroom">Mushrooms</input>' +
+          '<input type="checkbox" name="toppings" value="green-olive">Green Olive</input>' +
+          '<input type="checkbox" name="toppings" value="black-olive">Black Olive</input>' +
+          '<input type="checkbox" name="toppings" value="pineapple">Pineapple</input>' +
+        '</div>' +
       '</div>' +
     '</div>' +
   '</div>');
@@ -147,6 +127,49 @@ $(document).ready(function() {
 
   $("#add-pizza").click(function() {
     addFields();
+  });
+
+  $("form#order-form").submit(function(event) {
+    event.preventDefault();
+
+    var inputtedName = $("#name").val();
+
+    var inputtedDelivery = false;
+    var inputtedAddress = [];
+    var inputtedDeliveryOption = $("input:radio[name=delivery]:checked").val();
+    if(inputtedDeliveryOption === "delivery") {
+      inputtedDelivery = true;
+
+      var inputtedStreet = $("input#street").val();
+      var inputtedCity = $("input#city").val();
+      var inputtedState = $("input#state").val();
+      var inputtedZipCode = $("input#zip-code").val();
+
+      inputtedAddress.push(inputtedStreet, inputtedCity, inputtedState, inputtedZipCode);
+    }
+
+    var inputtedPizzas = [];
+
+    $(".pizza").each(function(){
+      var inputtedSize = parseInt($("input:radio[name=size]:checked").val());
+      var inputtedToppings = [];
+
+      $("input:checkbox[name=toppings]:checked").each(function(){
+        var topping = $(this).val();
+        inputtedToppings.push(topping);
+      });
+
+      var newPizza = new Pizza(inputtedSize, inputtedToppings);
+      newPizza.calculateCost();
+      inputtedPizzas.push(newPizza);
+    });
+
+    var newOrder = new Order(inputtedName, inputtedPizzas, inputtedDelivery);
+    newOrder.calculateCost();
+    newOrder.setAddress(inputtedAddress);
+    console.log(newOrder);
+    console.log(newOrder.pizzas[0]);
+    console.log(newOrder.pizzas[1]);
   });
 
 });
